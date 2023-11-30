2f909ac58544581f12d0ca0254a442013b7a865d
sebastian <sebastiankliem.sk@gmail.com>
2023-11-30 18:33:26 +0100

    add dropdown to menu

--- Scripts/navigation.js
+++ Scripts/navigation.js
@@ -1,5 +1,31 @@
-document.querySelectorAll('.menu-item').forEach(item => {
-    item.addEventListener('click', function() {
-        this.querySelector('.submenu').style.display = 'block';
+// document.querySelectorAll('.menu-item').forEach(item => {
+//     item.addEventListener('click', function() {
+//         this.querySelector('.submenu').style.display = 'block';
+//     });
+// });
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.menu-item').forEach(item => {
+        item.addEventListener('click', function(event) {
+            let submenu = this.querySelector('.submenu');
+            // Schaltet die Anzeige des Submenüs um
+            submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+            event.stopPropagation();
+        });
+    });
+
+    // Klick außerhalb des Menüs schließt das Submenü
+    document.addEventListener('click', function() {
+        document.querySelectorAll('.submenu').forEach(submenu => {
+            submenu.style.display = 'none';
+        });
     });
 });
+
+// Deaktiviert das Hover-Verhalten für kleine Bildschirme
+// window.addEventListener('resize', function() {
+//     if (window.innerWidth < 600) {
+//         document.querySelectorAll('.submenu').forEach(submenu => {
+//             submenu.style.display = 'none';
+//         });
+//     }
+// });
